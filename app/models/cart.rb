cb178c3f720e749b25fdd111ab2225fe4d31788e
Frank Condezo <frankcondezo@gmail.com>
2024-11-14 22:51:13 -0500

    update model cart and cart_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,11 @@
 class Cart < ApplicationRecord
-    has_many :cart_items, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+
+  def total
+    cart_items.to_a.sum { |cart_item| cart_item.total }
   end
+
+  def count
+    cart_items.to_a.sum { |cart_item| cart_item.quantity }
+  end
+end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,8 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
   validates :quantity, numericality: { greater_than: 0 }
+
+  def total
+    product.price * quantity
+  end
 end
